8d90c576e8116177ecac97312b5fb082375e2603
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-07-12 20:01:34 +0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import Events from './components/Events';
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom"
 
 
 function App() {
   return (
-    <>
-    <Events />
-    
-    
-    </>
-  );
+
+    <div>
+
+        {/* <Events /> */}
+        <Routes>
+
+          <Route path="events" element={<Events />} />
+
+        </Routes>
+      <Outlet />
+    </div>);
+
 }
 
 export default App;
